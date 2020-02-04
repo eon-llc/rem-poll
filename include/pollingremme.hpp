e7a357be9f3f9bf64eb1d731a521f60c285e6e4d
Serg Chernata <sergchernata@gmail.com>
2020-02-04 07:36:06 -0500

    added vote totals, token poll, poll description

--- include/pollingremme.hpp
+++ include/pollingremme.hpp
@@ -9,47 +9,84 @@ class [[eosio::contract("pollingremme")]] pollingremme : public eosio::contract 
   public:
 
     pollingremme(name receiver, name code, datastream<const char*> ds): contract(receiver, code, ds) {}
+
+    struct option {
+        string name;
+        option(string name) : name(name) {}
+        option() {}
+        EOSLIB_SERIALIZE(option, (name))
+    };
+
+    struct option_result : option {
+        uint64_t votes = 0;
+        option_result(const string& name, uint64_t votes) : option(name), votes(votes) {}
+        option_result(const string& name) : option_result(name, 0) {}
+        option_result() {}
+        EOSLIB_SERIALIZE(option_result, (name)(votes))
+    };
+
     typedef vector<string> option_list;
+    typedef vector<option_result> option_results;
 
     [[eosio::action]]
-    void create(name user, string subject, option_list options, bool is_token_poll, bool producers_only, bool guardians_only, time_point open_until);
+    void create(name user, string subject, string description, option_list options, bool is_token_poll, bool producers_only, bool guardians_only, time_point expires_at);
 
     [[eosio::action]]
     void vote(name user, uint64_t poll_id, uint8_t option_id);
 
+    [[eosio::action]]
+    void comment(name user, uint64_t poll_id, string message);
+
+    [[eosio::action]]
+    void deletedata();
+
   private:
+
+    static const uint32_t guardian_stake_threshold = 2500000000;
 
     static constexpr name system_account = name("rem");
 
     struct [[eosio::table]] poll_t {
-      uint64_t        id;
-      name            user;
-      string          subject;
-      option_list     options;
-      bool            is_token_poll;
-      bool            producers_only;
-      bool            guardians_only;
-      time_point      created_at;
-      time_point      expires_at;
+        uint64_t        id;
+        name            user;
+        string          subject;
+        string          description;
+        option_results  options;
+        bool            is_token_poll;
+        bool            producers_only;
+        bool            guardians_only;
+        time_point      created_at;
+        time_point      expires_at;
 
-      uint64_t        primary_key() const { return id; }
-      uint64_t        get_reverse_key() const { return ~id; }
+        uint64_t        primary_key() const { return id; }
+        uint64_t        get_reverse_key() const { return ~id; }
     };
 
     struct [[eosio::table]] vote_t {
-      uint64_t        poll_id;
-      name            user;
-      uint8_t         option_id;
-      time_point      created_at;
+        uint64_t        poll_id;
+        name            user;
+        uint8_t         option_id;
+        time_point      created_at;
 
-      uint64_t        primary_key() const { return poll_id; }
-      uint64_t        get_reverse_key() const { return ~poll_id; }
+        uint64_t        primary_key() const { return poll_id; }
+        uint64_t        get_reverse_key() const { return ~poll_id; }
+    };
+
+    struct [[eosio::table]] comment_t {
+        uint64_t        poll_id;
+        name            user;
+        string          message;
+        time_point      created_at;
+
+        uint64_t        primary_key() const { return poll_id; }
+        uint64_t        get_reverse_key() const { return ~poll_id; }
     };
 
     typedef multi_index<name("polls"), poll_t> polls_table;
     typedef multi_index<name("votes"), vote_t> votes_table;
+    typedef multi_index<name("comments"), comment_t> comments_table;
 
+    uint32_t get_voter_stake( const name& user ) const;
     bool is_producer( const name& user ) const;
-    bool is_active_producer( const name& user ) const;
-    bool is_voter( const name& user ) const;
+    bool is_guardian( const name& user ) const;
 };
--- src/pollingremme.cpp
+++ src/pollingremme.cpp
@@ -1,11 +1,12 @@
 #include <pollingremme.hpp>
 #include <rem.system/rem.system.hpp>
 
-void pollingremme::create(name user, string subject, option_list options, bool is_token_poll, bool producers_only, bool guardians_only, time_point expires_at) {
+void pollingremme::create(name user, string subject, string description, option_list options, bool is_token_poll, bool producers_only, bool guardians_only, time_point expires_at) {
 
   require_auth(user);
 
   check(!subject.empty(), "Poll subject can not be blank.");
+  check(subject.size() < 140, "Poll subject should be no longer than 140 characters.");
   check(options.size() > 1 && options.size() < 11, "A poll must have between 2 and 10 choices.");
 
   if(expires_at.sec_since_epoch() != 0) {
@@ -18,10 +19,18 @@ void pollingremme::create(name user, string subject, option_list options, bool i
     uint64_t id = polls.available_primary_key();
     time_point ct = current_time_point();
 
+    option_results results = {};
+    results.resize(options.size());
+    transform(options.begin(), options.end(), results.begin(), [&](string str) {
+        check(!str.empty(), "Option names can't be blank");
+        return option_result(str);
+    });
+
     p.id = id;
     p.user = user;
     p.subject = subject;
-    p.options = options;
+    p.description = description;
+    p.options = results;
     p.is_token_poll = is_token_poll;
     p.producers_only = producers_only;
     p.guardians_only = guardians_only;
@@ -37,32 +46,25 @@ void pollingremme::vote(name user, uint64_t poll_id, uint8_t option_id) {
   // check if poll exists and isn't expired
   polls_table polls(get_self(), get_self().value);
   const poll_t & p = polls.get(poll_id, "Poll with this id does not exist.");
-  check(p.expires_at.sec_since_epoch() > current_time_point().sec_since_epoch(), "Voting period for this poll has ended.");
+
+  // check poll expiration if it's set
+  if(p.expires_at.sec_since_epoch() != 0) {
+    check(p.expires_at.sec_since_epoch() > current_time_point().sec_since_epoch(), "Voting period for this poll has ended.");
+  }
 
   // check if voter qualifies for this poll
   if(p.producers_only) {
-
+    check(is_producer(user), "This poll requires block producer authorization.");
   }
 
   if(p.guardians_only) {
-
+    check(is_guardian(user), "This poll requires guardian authorization.");
   }
 
   // check if this voter can vote
   votes_table votes(get_self(), get_self().value);
   check(option_id < p.options.size(), "Option with this id does not exist in this poll.");
   check(votes.find(p.id) == votes.end(), "This account has already voted in this poll.");
-
-  if (p.is_token_poll) {
-
-    //token token("REM");
-    //print(token);
-    //asset balance = token.get_balance(voter, p.token.name());
-    //eosio_assert(balance.is_valid(), "Invalid token balance.");
-    //eosio_assert(balance.amount > 0, "Voter must have a stake of more than 0 tokens.");
-    //double stake = balance.amount / std::pow(10, stake.symbol.precision());
-    //print(stake);
-  }
 
   votes.emplace(user, [&](auto& v) {
     time_point ct = current_time_point();
@@ -72,21 +74,86 @@ void pollingremme::vote(name user, uint64_t poll_id, uint8_t option_id) {
     v.option_id = option_id;
     v.created_at = ct;
   });
+
+  polls.modify(p, user, [&](auto& p) {
+    if(p.is_token_poll) {
+        p.options[option_id].votes += get_voter_stake(user);
+    } else {
+        p.options[option_id].votes += 1;
+    }
+  });
+}
+
+void pollingremme::comment(name user, uint64_t poll_id, string message) {
+
+  require_auth(user);
+
+  // check if poll exists and isn't expired
+  polls_table polls(get_self(), get_self().value);
+  const poll_t & p = polls.get(poll_id, "Poll with this id does not exist.");
+
+  comments_table comments(get_self(), get_self().value);
+  check(!message.empty(), "Message can't be blank.");
+
+  comments.emplace(user, [&](auto& c) {
+    time_point ct = current_time_point();
+
+    c.poll_id = poll_id;
+    c.user = user;
+    c.message = message;
+    c.created_at = ct;
+  });
+}
+
+uint32_t pollingremme::get_voter_stake( const name& user ) const {
+    eosiosystem::voters_table _voters_table( system_account, system_account.value );
+    auto voter = _voters_table.find( user.value );
+
+    if(voter != _voters_table.end()) {
+        return voter->staked;
+    } else {
+        return 0;
+    }
 }
 
 bool pollingremme::is_producer( const name& user ) const {
-  eosiosystem::producers_table _producers_table( system_account, system_account.value );
-  return _producers_table.find( user.value ) != _producers_table.end();
+    eosiosystem::producers_table _producers_table( system_account, system_account.value );
+    auto prod = _producers_table.find( user.value );
+    return prod != _producers_table.end() && prod->is_active == 1;
 }
 
-bool pollingremme::is_active_producer( const name& user ) const {
-  eosiosystem::producers_table _producers_table( system_account, system_account.value );
-  return _producers_table.find( user.value ) != _producers_table.end();
+bool pollingremme::is_guardian( const name& user ) const {
+    eosiosystem::voters_table _voters_table( system_account, system_account.value );
+    auto voter = _voters_table.find( user.value );
+    bool exists = voter != _voters_table.end();
+    bool enough_staked = voter->staked >= guardian_stake_threshold;
+    bool is_active = voter->last_reassertion_time.sec_since_epoch() > (current_time_point().sec_since_epoch() - 60*60*24*30);
+    return exists && enough_staked && is_active;
 }
 
-bool pollingremme::is_voter( const name& user ) const {
-  eosiosystem::producers_table _producers_table( system_account, system_account.value );
-  return _producers_table.find( user.value ) != _producers_table.end();
+void pollingremme::deletedata() {
+    require_auth(get_self());
+
+    // delete polls
+    polls_table polls(get_self(), get_self().value);
+    auto p_itr = polls.begin();
+    while(p_itr != polls.end()){
+        p_itr = polls.erase(p_itr);
+    }
+
+    // delete votes
+    votes_table votes(get_self(), get_self().value);
+    auto v_itr = votes.begin();
+    while(v_itr != votes.end()){
+        v_itr = votes.erase(v_itr);
+    }
+
+    // delete comments
+    comments_table comments(get_self(), get_self().value);
+    auto c_itr = comments.begin();
+    while(c_itr != comments.end()){
+        c_itr = comments.erase(c_itr);
+    }
 }
 
-EOSIO_DISPATCH(pollingremme, (create)(vote))
+EOSIO_DISPATCH(pollingremme, (create)(vote)(comment)(deletedata))
